0f3647d583656762afd97a4ed716e34596e9842d
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-10-28 12:15:53 +0300

    Completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    sex TEXT NOT NULL,
+    color TEXT NOT NULL,
+    temperament TEXT NOT NULL,
+    alive BOOLEAN
+);
+
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+(1, "Mr. Chocolate", 20, "M", "brown", "calm", 0),
+(2, "Rowdy", 15, "M", "black", "intense", 1),
+(3, "Tabitha", 10, "F", "black", "nice", 1),
+(4, "Sargeant Brown", 19, "M", "brown", "moody", 0),
+(5, "Melissa", 24, "F", "white", "goofy", 1),
+(6, "Grinch", 30, "M", "black", "cool", 1),
+(7, "Wendy", 8, "F", "white", "naive", 1),
+(8, NULL, 17, "M", "black", "aggressive", 0);
+
